1bcfc17c4407035134fffea8a92fa9cf22d14948
JH0726 <Johnathan.Huang@education.nsw.gov.au>
2024-05-13 11:53:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import pygame
 import sys
 import os
 import turtle
-import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import scipy
-import SpacePy
+import spacepy
 import poliastro
 import astropy
 import numpy
 import pandas
+import astroquery
+
+p.r += p.v * dt 
+acc = -2.959e-4 * p.r / np.sum(p.r**2)**(3./2) # in units of AU/day^2
+p.v += acc * dt

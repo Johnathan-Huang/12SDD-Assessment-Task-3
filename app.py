b3658720c7a2e2ee1846e0a498adbe6d3d573d6b
JH0726 <Johnathan.Huang@education.nsw.gov.au>
2024-05-21 10:38:52 +1000

    Update app.py

--- app.py
+++ app.py
@@ -15,37 +15,3 @@ from astroquery.jplhorizons import Horizons
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget, QPushButton, QFileDialog
 
-def window():
-    app = QApplication(sys.argv)
-    win = MyWindow()
-    win.show()
-    sys.exit(app.exec_())
-
-def clicked():
-    print(" Button Clicked ")
-
-class MyWindow(QMainWindow):
-    def __init__(self):
-        super(MyWindow, self).__init__()
-        self.setGeometry(500, 500, 1920, 1080)
-        self.setWindowTitle(" Space Simulation ")
-        self.initUI()
-
-    def initUI(self):
-        self.label = QtWidgets.QLabel(self)
-        self.label.setText(" Placeholder ")
-        self.label.move(10,10)
-
-        self.b1 = QtWidgets.QPushButton(self)
-        self.b1.setText(" Button 1 ")
-        self.b1.move(50,50)
-        self.b1.clicked.connect(self.clicked)
-
-    def clicked(self):
-        self.label.setText(" Button Clicked ")
-        self.update()
-
-    def update(self):
-        self.label.adjustSize()
-
-window()

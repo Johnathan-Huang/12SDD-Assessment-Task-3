72686329cca68d212e6a210ba7362485b2328d6a
JH0726 <Johnathan.Huang@education.nsw.gov.au>
2024-05-29 10:13:18 +1000

    added the planet class for the simulation

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from astropy.time import Time
 from astroquery.jplhorizons import Horizons
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget, QPushButton, QFileDialog
+import math
 
 pygame.init()
 pygame.font.init()
@@ -92,6 +93,80 @@ def sim_loop():
             if event.type == pygame.VIDEORESIZE:
                 WIDTH, HEIGHT = event.w, event.h
                 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+
+distance_font = pygame.font.Font("Abel.ttf", 50)
+class Planet:
+	AU = 149.6e6 * 1000
+	G = 6.67428e-11
+	SCALE = 250 / AU  # 1AU = 100 pixels
+	TIMESTEP = 3600*24 # 1 day
+
+	def __init__(self, x, y, radius, color, mass):
+		self.x = x
+		self.y = y
+		self.radius = radius
+		self.color = color
+		self.mass = mass
+
+		self.orbit = []
+		self.sun = False
+		self.distance_to_sun = 0
+
+		self.x_vel = 0
+		self.y_vel = 0
+
+	def draw(self, win):
+		x = int(self.x * self.SCALE + WIDTH / 2)
+		y = int(self.y * self.SCALE + HEIGHT / 2)
+
+		if len(self.orbit) > 2:
+			updated_points = []
+			for point in self.orbit:
+				x, y = point
+				x = int(x * self.SCALE + WIDTH / 2)
+				y = int(y * self.SCALE + HEIGHT / 2)
+				updated_points.append((x, y))
+
+			pygame.draw.aalines(win, self.color, False, updated_points, 1920)
+
+		pygame.gfxdraw.aacircle(win, x, y, self.radius, self.color)
+		pygame.gfxdraw.filled_circle(win, x, y, self.radius, self.color)
+		
+		if not self.sun:
+			distance_text = distance_font.render(f"{round(self.distance_to_sun/1000, 1)}km", 1, WHITE)
+			win.blit(distance_text, (x - distance_text.get_width()/2, y - distance_text.get_height()/2))
+
+	def attraction(self, other):
+		other_x, other_y = other.x, other.y
+		distance_x = other_x - self.x
+		distance_y = other_y - self.y
+		distance = math.sqrt(distance_x ** 2 + distance_y ** 2)
+
+		if other.sun:
+			self.distance_to_sun = distance
+
+		force = self.G * self.mass * other.mass / distance**2
+		theta = math.atan2(distance_y, distance_x)
+		force_x = math.cos(theta) * force
+		force_y = math.sin(theta) * force
+		return force_x, force_y
+
+	def update_position(self, planets):
+		total_fx = total_fy = 0
+		for planet in planets:
+			if self == planet:
+				continue
+
+			fx, fy = self.attraction(planet)
+			total_fx += fx
+			total_fy += fy
+
+		self.x_vel += total_fx / self.mass * self.TIMESTEP
+		self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+		self.x += self.x_vel * self.TIMESTEP
+		self.y += self.y_vel * self.TIMESTEP
+		self.orbit.append((self.x, self.y))
 
 def play_game():
     global screen, WIDTH, HEIGHT # Declare global variables

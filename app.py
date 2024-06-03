e88110a7f7ac46e50d26d5cff57922ebaf07b950
JH0726 <Johnathan.Huang@education.nsw.gov.au>
2024-06-03 10:53:51 +1000

    trying to get a good icon

--- app.py
+++ app.py
@@ -28,15 +28,19 @@ WHITE = (255, 255, 255)
 GREEN = (0, 100, 0)
 RED = (100, 0, 0)
 BLUE = (0, 0, 100)
+YELLOW = (255, 255, 0)
+GREY = (100, 100, 100)
+BROWN = (139, 69, 19)
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 pygame.display.set_caption("Sol Simulation")
+pygame.display.set_icon(pygame.image.load("images/literallysun.png"))
 
 def main():
 	run = True
 	clock = pygame.time.Clock()
 
-	sun = Planet(0, 0, 30, WHITE, 1.98892 * 10**30)
+	sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
 	sun.sun = True
 
 	earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
@@ -45,10 +49,10 @@ def main():
 	mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
 	mars.y_vel = 24.077 * 1000
 
-	mercury = Planet(0.387 * Planet.AU, 0, 8, BACKGROUND_COLOUR, 3.30 * 10**23)
+	mercury = Planet(0.387 * Planet.AU, 0, 8, GREY, 3.30 * 10**23)
 	mercury.y_vel = -47.4 * 1000
 
-	venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+	venus = Planet(0.723 * Planet.AU, 0, 14, BROWN, 4.8685 * 10**24)
 	venus.y_vel = -35.02 * 1000
 
 	planets = [sun, earth, mars, mercury, venus]
@@ -137,12 +141,13 @@ def sim_loop():
                 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
 
         screen.fill(BACKGROUND_COLOUR_2)
+        main()
         pygame.display.flip()
-        		
+        
 def save_screen():
 	pass
 	
-distance_font = pygame.font.Font("Abel.ttf", 50)
+distance_font = pygame.font.Font("Abel.ttf", 25)
 class Planet:
 	AU = 149.6e6 * 1000
 	G = 6.67428e-11
